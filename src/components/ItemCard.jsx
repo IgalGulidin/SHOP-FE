8f24ae30aaf00673726cbe781e244109fb9ca08b
Igal Gulidin <gulidinigal@gmail.com>
2026-03-11 14:32:03 +0200

    ItemCard additions

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -6,7 +6,7 @@ import {
   Button,
 } from "@mui/material";
 
-export default function ItemCard({ item, onAddFavorites, onAddToCart }) {
+export default function ItemCard({ item, onAddFavorite, onRemoveFavorite, onAddToCart }) {
   return (
     <Card sx={{ width: 300 }}>
       <CardMedia
@@ -15,31 +15,44 @@ export default function ItemCard({ item, onAddFavorites, onAddToCart }) {
         image={item.imageUrl}
         alt={item.titke}
       />
+
       <CardContent>
         <Typography variant="h6">{item.title}</Typography>
         <Typography>${item.priceUsd}</Typography>
+        
         <Typography color={item.stockQty === 0 ? "error" : "text.secondary"}>
           {item.stockQty === 0
             ? "0 items left in stock"
             : `${item.stockQty} items in stock`}
         </Typography>
 
-        <Button
-          variant="contained"
-          sx={{ mt: 1, mr: 1 }}
-          onClick={() => onAddFavorites(item.id)}
-        >
-          Add to Favorites
-        </Button>
+        <Box sx={{ mt: 2, display: "flex", gap: 1, flexWrap: "wrap" }}>
+            {onAddFavorite && (
+              <Button varaint="contained" onClick={() => onAddFavorite(item.id)}>
+                Add to Favorites
+              </Button>
+            )}
 
-        <Button
-          variant="outlined"
-          sx={{ mt: 1 }}
-          disabled={item.stockQty === 0}
-          onClick={() => onAddToCart(item.id)}
-        >
-          Add to Cart
-        </Button>
+            {onRemoveFavorite && (
+              <Button 
+                variant="outlined"
+                color="error"
+                onClick={() => onRemoveFavorite(item.id)}
+              >
+                Remove Favorite
+              </Button>
+            )}
+            
+            {onAddToCart && (
+              <Button
+                variant="outlined"
+                disabled={item.stockQty === 0}
+                onClick={() => onAddToCart(item.id)}
+              >
+                Add to Cart
+              </Button>
+            )}
+        </Box>
       </CardContent>
     </Card>
   );
